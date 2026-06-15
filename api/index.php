6d8577bfd9aa651b3c129c26f580256a31524a8e
askiri <askirisaipulam2@gmail.com>
2026-06-15 13:32:38 +0700

    8

--- api/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Meneruskan permintaan Vercel ke berkas index.php bawaan Laravel
-require __DIR__ . '/../public/index.php';
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+// Mengalihkan seluruh request serverless Vercel ke file index bawaan Laravel
+require __DIR__ . '/public/index.php';
